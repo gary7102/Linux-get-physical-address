4de9c60e3fb94fd291fc531051e790feca13ba96
gary7102 <160698915+gary7102@users.noreply.github.com>
2024-11-14 23:15:58 +0800

    Create page_fault.c

---
+++ user space/page_fault.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <sys/syscall.h> /* Definition of SYS_* constants */
+#include <unistd.h>
+
+
+
+
+                void *my_get_physical_addresses(void *vaddr)
+                {
+                    unsigned long paddr;
+                    long result = syscall(450, &vaddr, &paddr); // Call the syscall with vaddr
+
+                    if (result == -1) { // Check for errors
+                         perror("syscall failed");
+                         return NULL; // Handle error appropriately
+                    }
+                    return (void *)paddr; // Return the physical address
+                }
+
+
+                 int a[2000000];   // store int bss segment same as 
+                                   // int a[2000000] = {0};
+
+
+                 void  main()
+                 {
+                   int      loc_a;
+                   void     *phy_add;
+
+                   for(int i=0; i<2000000; i++){
+                           a[i] = 0;
+                   }
+
+                   for(int i=0; i<2000000; i++){
+                        phy_add = my_get_physical_addresses(&a[i]);
+                        if(phy_add == NULL){
+                                printf("stop in a[%d], logical address = [%p]\n", i, &a[i]);
+                                break;
+                        }
+                   //     printf("a[%d] logical address: [%p], physical address: [%p]\n", i, &a[i], phy_add);
+                   }
+                   printf("already print all the element! \n");
+                 }
